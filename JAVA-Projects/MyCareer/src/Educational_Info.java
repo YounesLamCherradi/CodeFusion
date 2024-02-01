14ad35932336910b2f74a55c083f9e9c59f11765
YounesLamCherradi <157892634+YounesLamCherradi@users.noreply.github.com>
2024-02-02 01:17:59 +0300

    Update Educational_Info.java

--- JAVA-Projects/MyCareer/src/Educational_Info.java
+++ JAVA-Projects/MyCareer/src/Educational_Info.java
@@ -1,1 +1,32 @@
+package ACCOUNT;
+
+public class Educational_Info {
+
+	public String Latest_degree;
+	public String Discipline;
+	public int Grad_year;
+	public Profile profile;
+	public Educational_Info(String latest_degree, String discipline, int grad_year,Profile profile) {
+		Latest_degree = latest_degree;
+		Discipline = discipline;
+		Grad_year = grad_year;
+		this.profile=profile;
+	}
+	
+	public void Update_edu_info(String latest_degree,String discipline,int grad_year) {
+		Latest_degree=latest_degree;
+		Discipline=discipline;
+		Grad_year=grad_year;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Educational_Info [Latest_degree=" + Latest_degree + ", Discipline=" + Discipline + ", Grad_year="
+				+ Grad_year + "]";
+	}
+	
+	
+	
+}
 
